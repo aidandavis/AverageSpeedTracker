2fcb807058e8a4defde1e22e5897ed28a9310df7
Aidan Davis <aidan.davis94@gmail.com>
2017-11-25 14:54:02 +0800

    add start of activity layout

--- app/src/main/java/com/aidandavisdev/aidandavis/averagespeedtracker/MainActivity.kt
+++ app/src/main/java/com/aidandavisdev/aidandavis/averagespeedtracker/MainActivity.kt
@@ -2,10 +2,16 @@ package com.aidandavisdev.aidandavis.averagespeedtracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) // keep screen on
+        setContentView(R.layout.activity_main)
+
+
     }
 }
